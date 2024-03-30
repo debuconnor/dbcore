f7d2c6afa47bdd2a4f7f4bfd1868fd86da5a896f
debuconnor <gterppw@gmail.com>
2024-03-30 17:14:19 +0900

    change error code to string to handle err

--- vars.go
+++ vars.go
@@ -2,9 +2,9 @@ package dbcore
 
 const (
 	APP_NAME                               = "DBCORE"
-	ERROR_CODE_DB_CONNECTION_FAILED        = 100
-	ERROR_CODE_CREATE_SECRETMANAGER_CLIENT = 101
-	ERROR_CODE_ACCESS_SECRET_VERSION       = 102
-	ERROR_CODE_DATA_CORRUPTION             = 103
-	ERROR_CODE_PARSE_JSON                  = 104
+	ERROR_CODE_DB_CONNECTION_FAILED        = "Failed to connect to database."
+	ERROR_CODE_CREATE_SECRETMANAGER_CLIENT = "Failed to create SecretManager client."
+	ERROR_CODE_ACCESS_SECRET_VERSION       = "Failed to access secret version."
+	ERROR_CODE_DATA_CORRUPTION             = "Secret data corruption."
+	ERROR_CODE_PARSE_JSON                  = "Failed to parse JSON."
 )
