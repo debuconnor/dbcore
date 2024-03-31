24d2c63df6984f5350d29278991e0eb90807486b
debuconnor <gterppw@gmail.com>
2024-03-31 23:38:41 +0900

    update .gitignore

---
+++ vars.go
@@ -0,0 +1,12 @@
+package dbcore
+
+const (
+	APP_NAME = "DBCORE"
+
+	ERROR_DB_CONNECTION_FAILED        = "Failed to connect to database."
+	ERROR_CREATE_SECRETMANAGER_CLIENT = "Failed to create SecretManager client."
+	ERROR_ACCESS_SECRET_VERSION       = "Failed to access secret version."
+	ERROR_DATA_CORRUPTION             = "Secret data corruption."
+	ERROR_PARSE_JSON                  = "Failed to parse JSON."
+	ERROR_INVALID_QUERY               = "Invalid query."
+)
